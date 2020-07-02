24a57da073f3a43ae9f39d1e6e67cecc5411dd12
evagorac <evagorac@bxscience.edu>
2020-07-01 20:10:03 -0400

    Done

---
+++ catking_ws/build/RobotController/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris/PycharmProjects/TBrain/catking_ws/src/RobotController
+# Build directory: /home/chris/PycharmProjects/TBrain/catking_ws/build/RobotController
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catking_ws/build/TBrain/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris/PycharmProjects/TBrain/catking_ws/src/TBrain
+# Build directory: /home/chris/PycharmProjects/TBrain/catking_ws/build/TBrain
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
