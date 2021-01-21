3e951b1e8d89945716f2a242ef9befd54458bdb7
itsme <evagorac@bxscience.edu>
2021-01-21 23:23:41 +0000

    added python scripts

---
+++ src/clear_errors.py
@@ -0,0 +1,9 @@
+import setup
+
+odrives = setup.import_odrives("odrive_serials.txt")
+
+for odrive in odrives:
+  print("clearing errors for odrive " + str(odrive.serial_number))
+  setup.clear_errors(odrive)
+  print()
+  print()
