7f01ac5bb1e7e967ae9c81dd6eae35333cba1984
itsme <evagorac@bxscience.edu>
2021-01-22 02:12:09 +0000

    pose wrapper tested

--- src/interface.py
+++ src/interface.py
@@ -1,23 +1,31 @@
 import numpy as np
 
-class Pose_Wrapper:
+class PoseWrap:
   # matrix pose handler
   # constuctor requires a rotation matrix and position vector
-  def __init__(self, r_matrix=np.identity(3), pos_vec=np.zeros((3,1))):
-    self.__pose = np.block([[r_matrix, pos_vec], [0, 0, 0, 1]])
+  def __init__(self, pose):
+    pose = np.array(pose)
+    self.check_pose_legality(pose)
+    self.__pose = pose
 
-  def set_r_matrix(self, r_matrix)
+  def set_pose(self, pose):
+    pose = np.array(pose)
+    self.check_pose_legality(pose)
+    self.__pose = pose
+
+  def set_r_matrix(self, r_matrix):
+    r_matrix = np.array(r_matrix)
     if ~self.is_r_matrix_legal(r_matrix):
       raise Exception("rotaion matrix is illegal " + str(r_matrix))
     self.__pose[:3, :3] = r_matrix
 
-  def set_pos(self, pos_vec)
-    if pos_vec.shape != (3,1) or np.any(~np.isreal(pos_vec))
+  def set_pos(self, pos_vec):
+    pos_vec = np.array(pos_vec)
+    if pos_vec.shape != (3,1) or np.any(~np.isreal(pos_vec)):
       raise Exception("position vector is illegal " + str(pos_vec))
     self.__pose[:3, 3] = pos_vec
 
-  @staticmethod
-  def is_r_matrix_legal(r_matrix):
+  def is_r_matrix_legal(self, r_matrix):
     if r_matrix.shape != (3,3):
       return False
     # check if r_matrix is orthogonal by dotting all col vecs and making sure determinant == 1
@@ -30,37 +38,47 @@ class Pose_Wrapper:
           return False
     return True
 
-  @staticmethod
-  def check_pose_legality(pose):
+  def check_pose_legality(self, pose):
+    pose = np.array(pose)
+    # pose is np 4x4 trans matrix
+    # check to see if pose is legal
+    if pose.shape != (4,4):
+      raise Exception("pose is not a 4x4 matrix " + str(pose))
+
     # check for real numbers
     if np.any(~np.isreal(pose)):
       raise Exception("nonreal numbers present in pose matrix " + str(pose))
 
-    r_matrix = pose.get_r_matrix()
-    if ~is_r_matrix_legal(r_matrix)
-      raise Exception("rotation matrix is non orthogonal " + str(r_matrix))
+    # ensure rotation matrix is legal
+    r_matrix = pose[:3, :3]
+    if not self.is_r_matrix_legal(r_matrix):
+      raise Exception("rotation matrix is illegal " + str(r_matrix))
 
-    if pose[3, :] != np.array([0, 0, 0, 1]):
+    # ensure the bottom row of transformation matrix is correct
+    if np.any(~np.equal(pose[3, :], np.array([0, 0, 0, 1]))):
       raise Excpetion("transformation matrix is nonhomogenous " + str(pose))
 
   def forward_transform(self, ext_pose):
-    if ~isinstance(ext_pose, Pose_Wrapper):
-      raise Exception("pose object passed to forward transform is not a Pose_Wrapper instance")
-    self.__pose = np.matmul(ext_pose.get_pose(), self.__pose)
+    if not isinstance(ext_pose, PoseWrap):
+      raise Exception("pose object passed to forward transform is not a PoseWrap instance")
+    return np.matmul(ext_pose.get_pose(), self.__pose)
 
   def inverse_transform(self, ext_pose):
-    if ~isinstance(ext_pose, Pose_Wrapper):
-      raise Exception("pose object passed to inverse transform is not a Pose_Wrapper instance")
-    self.__pose = np.linalg.solve(ext_pose.get_pose(), self.__pose)
+    if not isinstance(ext_pose, PoseWrap):
+      raise Exception("pose object passed to inverse transform is not a PoseWrap instance")
+    return np.linalg.solve(ext_pose.get_pose(), self.__pose)
 
-  def get_pose():
+  def get_pose(self):
     return self.__pose
 
-  def get_r_matrix():
+  def get_r_matrix(self):
     return self.__pose[:3, :3]
 
-  def get_pos_vec():
-    return self.__pose[:3, 3]
+  def get_pos_vec(self):
+    return self.__pose[:3, 3].reshape((3,1))
+
+  def __str__(self):
+    return str(self.__pose)
 
 
 class Robot:
@@ -125,7 +143,14 @@ class JointController:
     pass
 
 
-if __name__ == __main__:
+if __name__ == "__main__":
   print("running test cases")
   print("start with pose wrapper")
-  
+
+  t1 = PoseWrap([[0,1,0,1],[1,0,0,1],[0,0,-1,1],[0,0,0,1]])
+  x = np.array([[0,1,0,1],[1,0,0,1],[0,0,-1,1],[0,0,0,1]])
+  t2 = PoseWrap(x)
+  res = t2.forward_transform(t1)
+  print(t1)
+  print(t2)
+  print(res)
