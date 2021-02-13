79ba3d20d35be061514a1eaa003c6e149e621f8d
itsme <evagorac@bxscience.edu>
2021-02-13 05:26:20 +0000

    fixed up inputs

--- src/utils/odriveIOtest.py
+++ src/utils/odriveIOtest.py
@@ -1,7 +1,7 @@
 import odrive as od
 import setup
 
-''' we will test the funcitonality of the GPIO pins on the odrives for ease of wiring, all inputs to the system (homing switches, etc.) are connected to the top odrive on the stack.  we will print the state of each input (currently 6 for each joint homing switch)'''
+# test functionality of inputs.  each input is connected to GPIO 3 and 4 of each odrive.
 
 odrives = setup.import_odrives("../odrive_serials.txt")
 
@@ -9,9 +9,9 @@ while True:
     volts = []
     for odrive in odrives:
         for pin in range(3,5):
-            volts.append(odrive.get_adc_voltage(3,4))
+            volts.append(odrive.get_adc_voltage(pin))
     for val in volts:
-        print(val)
+        print(val > 1.5)
     print()
 
 
