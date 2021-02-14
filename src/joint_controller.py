fea7c6a0bdc1df2351bc14397db4bcc4752c50e3
itsme <evagorac@bxscience.edu>
2021-02-14 18:07:27 +0000

    updated controller

--- src/joint_controller.py
+++ src/joint_controller.py
@@ -1,28 +1,45 @@
 import asyncio
 import websockets
 import pickle
-from utils import setup
-from utils import interface
-import numpy
+#from utils import interface
+import numpy as np
 import odrive as od
 
 
 async def server(websocket, path):
-    cmd = await websocket.recv()
-    print(f"received command {cmd}")
-    do(cmd)
+    global joint_cmd
+    p_cmd = await websocket.recv()
+    cmd = pickle.loads(p_cmd)
+    print('received', cmd)
+
+    valid = True
+    for i in range(len(cmd)):
+        if np.abs(cmd[i] - joint_cmd[i]) > max_step:
+            valid = False
+    if not valid:
+        print('max step exceeded, returning current angles', joint_cmd)
+        p_joint_cmd = pickle.dumps(joint_cmd)
+        await websocket.send(p_joint_cmd)
+    else:
+        p_succ = pickle.dumps('k')
+        joint_cmd = cmd
+        await websocket.send(p_succ)
+        do(cmd)
 
 def do(cmd):
-    joint, vel_dir = cmd
+    for x in cmd:
+        print(x, 'turns')
+    print()
+    #joint, vel_dir = cmd
+    #JC.simple_set(setpoints, testing=True)
 
-    
-
-    JC.simple_set(setpoints, testing=True)
-
+joint_cmd = [0,0,0,0,0,0]
+max_step = 2 * (1/4 * 25 / 15)
+print('max step allowable', max_step)
 
 OD_serial_path = '../odrive_serials.txt'
-JC = interface.JointController(OD_serial_path)
-JC.calib_axes()
+#JC = interface.JointController(OD_serial_path)
+#JC.calib_axes()
 
 start_server = websockets.serve(server, "192.168.1.95", 8765)
 
