3db16a62c0255a25fd7f83a86c90fd5a9ff830f1
evagorac <evagorac@bxscience.edu>
2021-02-13 23:06:18 -0500

    client

--- src/tests/websocket_client.py
+++ src/tests/websocket_client.py
@@ -1,15 +1,19 @@
 import asyncio
 import websockets
+import pickle
 
 async def hello():
-    uri = "ws://localhost:8765"
+    uri = "ws://192.168.1.95:8765"
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
 
-        await websocket.send(name)
-        print(f"> {name}")
+        joint_vel_cmds = [0,0,0,0,0,0]
+        print(f'attempting to send {joint_vel_cmds}')
+        pic_cmd = pickle.dumps(joint_vel_cmds)
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
+        await websocket.send(pic_cmd)
+
+        success = await websocket.recv()
+        if success == '1':
+            print('success')
 
 asyncio.get_event_loop().run_until_complete(hello())
